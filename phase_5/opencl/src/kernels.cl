7fa020eba1b7b1444812eb08e6a8418a4a577f91
jruntti20 <jruntti20@edu.oulu.fi>
2026-03-21 22:19:25 +0200

    Resize and grayscale, cross checking and occlusion filling kernels added and works, some ugly comment lines exist still. Commandline parameters should be added next.

--- phase_5/opencl/src/kernels.cl
+++ phase_5/opencl/src/kernels.cl
@@ -1,5 +1,133 @@
-#define MAX_DISPARITY 256
-#define WINDOW 5
+#define MAX_DISPARITY 64
+#define BLOCK_SIZE 16
+typedef unsigned char BYTE;
+
+
+#define DEBUG_BUFFER_SIZE 8192
+
+inline int dbg_alloc(__global int* index, int length)
+{
+    return atomic_add(index, length);
+}
+
+inline void dbg_write_str(__global char *buf, int offset, __constant char *str)
+{
+    int i = 0;
+    while (str[i] != '\0')
+    {
+        buf[offset + i] = str[i];
+        i++;
+    }
+}
+
+inline int dbg_write_int(__global char *buf, int offset, int value)
+{
+    int start = offset;
+
+    if (value == 0)
+    {
+        buf[offset++] = '0';
+        return 1;
+    }
+    if (value < 0)
+    {
+        buf[offset++] = '-';
+        value = -value;
+    }
+    int digits[10];
+    int d = 0;
+
+    while(value > 0)
+    {
+        digits[d++] = value % 10;
+        value /= 10;
+    }
+
+    for (int i = d - 1; i >= 0; i--)
+    {
+    buf[offset++] = '0' + digits[i];
+    }
+    return offset - start;
+}
+
+inline int dbg_write_float(__global char *buf, int offset, float value)
+{
+    int start = offset;
+
+    int int_part = (int)value;
+    float frac = fabs(value - (float)int_part);
+
+    offset += dbg_write_int(buf, offset, int_part);
+
+    buf[offset++] = '.';
+
+    // 3 decimals
+    for(int i = 0; i < 3; i++)
+    {
+        frac *= 10.0f;
+        int digit = (int)frac;
+        buf[offset++] = '0' + digit;
+        frac -= digit;
+    }
+
+    return offset - start;
+}
+
+inline void dbg_printf(__global char *buf, __global int* index, __constant char *fmt, float a, float b, float c)
+{
+    int offset = dbg_alloc(index, 128);
+    int i = 0;
+    int arg_i = 0;
+
+    while (fmt[i] != '\0')
+    {
+        if (fmt[i] == '%')
+        {
+            i++;
+
+            if (fmt[i] == 'f')
+            {
+                switch(arg_i)
+                {
+                    case 0:
+                        offset += dbg_write_float(buf, offset, a);
+                        break;
+                    case 1:
+                        offset += dbg_write_float(buf, offset, b);
+                        break;
+                    case 2:
+                        offset += dbg_write_float(buf, offset, c);
+                        break;
+                }
+                arg_i++;
+            }
+            //else if (fmt[i] == 'd')
+            //{
+            //    offset += dbg_write_int(buf, offset, c);
+            //}
+        }
+        else
+        {
+            buf[offset++] = fmt[i];
+        }
+        i++;
+    }
+    buf[offset++] = '\n';
+    buf[offset] = '\0';
+}
+
+__kernel void testKernel(__global char *debug_buf, __global int *debug_index)
+{
+    int gid_y = get_global_id(0);
+    int gid_x = get_global_id(1);
+
+    int size_y = get_global_size(0);
+    int size_x = get_global_size(1);
+
+    float val = gid_x * 1.234f;
+    //dbg_printf(debug_buf, debug_index, "g_size_x=%d val=%f gid_x=%d", size_x, val, gid_x);
+    //dbg_printf(debug_buf, debug_index, "gid_x: %f, gid_y %f, size_x: %f\n", gid_x, gid_y, size_x);
+}
 
 __kernel void zncc_fast(
     __global uchar* left,
@@ -7,44 +135,82 @@ __kernel void zncc_fast(
     __global uchar* disparity,
     int disp_sign,
     int width,
-    int height)
+    int height,
+    int window,
+    __global char *debug_buf,
+    __global int *debug_index)
 {
     int x = get_global_id(0);
     int y = get_global_id(1);
+    
+    int group_x = get_group_id(0);
+    int group_y = get_group_id(1);
 
-    if(x < WINDOW || y < WINDOW || x >= width-WINDOW || y >= height-WINDOW)
+    int group_x_size = get_num_groups(0);
+    int group_y_size = get_num_groups(1);
+
+    int size_x = get_global_size(0);
+    int size_y = get_global_size(1);
+
+
+    //disparity[y*width+x] = 128;
+    //return;
+
+    //if (x == 0 && y == 0)
+    //{
+    //    dbg_printf(debug_buf, debug_index, "x: %f, y %f, size_x: %f\n", x, y, size_x);
+    //}
+
+    if(x>=width || y >= height)
+        return;
+
+    if(x < window || y < window || x >= width-window || y >= height-window)
     {
         disparity[y*width+x] = 0;
         return;
     }
 
-    int best_d = 0;
-    float best_score = -1.0f;
+    //if (!(x + (d * disp_sign) < width - window))
+    //    return;
 
-    for(int d=0; d<MAX_DISPARITY && x+d < width-WINDOW; d++)
+    int best_d = 0;
+    float best_score = -2.0f;
+
+    for(int d=0; d<MAX_DISPARITY && x+d < width-window; d++)
     {
+
         float sumL=0,sumR=0,sumLR=0,sumL2=0,sumR2=0;
         int count=0;
 
-        for(int wy=-WINDOW; wy<=WINDOW; wy++)
-        for(int wx=-WINDOW; wx<=WINDOW; wx++)
+        for(int wy=-window; wy<=window; wy++)
+        for(int wx=-window; wx<=window; wx++)
         {
             int xl=x+wx;
             int yl=y+wy;
             int xr=xl-(d*disp_sign);
 
-            int idxL=((yl*width+xl)*4);
-            int idxR=((yl*width+xr)*4);
+            int idxL=yl*width+xl;
+            int idxR=yl*width+xr;
 
-            float L =
-            0.299f*left[idxL] +
-            0.587f*left[idxL+1] +
-            0.114f*left[idxL+2];
+            //int xr = x + wx - d * disp_sign;
+            //if(xr < 0 || xr >= width)
+            //    continue;
 
-            float R =
-            0.299f*right[idxR] +
-            0.587f*right[idxR+1] +
-            0.114f*right[idxR+2];
+            //if (x == 100 && y == 100)
+            //{
+            //    dbg_printf(debug_buf, debug_index, "d: %f, idxL: %f, idxR: %f\n", d, idxL, idxR);
+            //}
+            
+            float L = left[idxL];
+            //float L = (float)tileL[ty * tile_w + tx];
+
+            float R = right[idxR];
+            //float R = (float)tileR[ty * tile_w + tx_r];
+
+            //if (x == 100 && y == 100)
+            //{
+            //    dbg_printf(debug_buf, debug_index, "d: %f, L: %f, R: %f\n", d, L, R);
+            //}
 
             sumL+=L;
             sumR+=R;
@@ -55,11 +221,16 @@ __kernel void zncc_fast(
             count++;
         }
 
+        if(count == 0)
+            continue;
+
         float numerator = count*sumLR - sumL*sumR;
         float denomL = count*sumL2 - sumL*sumL;
         float denomR = count*sumR2 - sumR*sumR;
 
-        if(denomL < 1e-5f || denomR < 1e-5f)
+        float denom = denomL * denomR;
+
+        if(denom < 1e-5f)
             continue;
 
         float score = numerator / sqrt(denomL * denomR);
@@ -69,15 +240,108 @@ __kernel void zncc_fast(
             best_score = score;
             best_d = d;
         }
+        
+        //if (x == 100 && y == 100)
+        //{
+        //    dbg_printf(debug_buf, debug_index, "d: %f, numerator: %f, score: %f\n", d, numerator, score);
+        //}
     }
 
-    disparity[y*width+x] = abs(best_d) * 255 / MAX_DISPARITY;
+    //disparity[y*width+x] = (uchar)(best_d * 255 / MAX_DISPARITY);
+    disparity[y*width+x] = best_d;
 }
 
-__kernel void resize_image(__global uchar* buff)
+__kernel void resize_grayscale_image(__global const BYTE *inputBuf1, __global const BYTE *inputBuf2, __global BYTE *outputBuf1, __global BYTE *outputBuf2, uchar resize_factor, uint w)
 {
+    int i = get_global_id(0);
+    int j = get_global_id(1);
+    uint new_w = w/resize_factor;
+    ulong idx = ((unsigned int)(i * w + j)) * 4 * resize_factor; // Take resize_factor:th pixels (4 bytes per pixel) from original image
+    outputBuf1[i*new_w + j] = (BYTE)(inputBuf1[idx]*0.299 + inputBuf1[idx + 1]*0.587 + inputBuf1[idx + 2] * 0.114);
+    outputBuf2[i*new_w + j] = (BYTE)(inputBuf2[idx]*0.299 + inputBuf2[idx + 1]*0.587 + inputBuf2[idx + 2] * 0.114);
 }
 
-__kernel void grayscale_image()
+__kernel void cross_checking(__global BYTE *inputImageL, __global BYTE *inputImageR, __global BYTE *outputImgL, int width, int height, int threshold, int win_size, __global char *debug_buf, __global int *debug_index)
 {
+    int i = get_global_id(1);
+    int j = get_global_id(0);
+
+    //if (i == 0 && j == 0)
+    //{
+    //    dbg_printf(debug_buf, debug_index, "Cross checking: i: %f, j %f, 1: %f\n", i, j, 1);
+    //}
+
+    int idx = j+i*width;
+
+    int dL = inputImageL[idx];
+
+    int jr = j - dL; // corresponding pixel in right image
+    
+    if(jr < 0 || jr >= width)
+    {
+        outputImgL[idx] = 0;
+        outputImgL[idx] = inputImageL[idx];
+        return;
+    }
+
+    int dR = inputImageR[jr+i*width];
+
+    if (abs(dL - dR) > threshold)
+    {
+        outputImgL[idx] = 0;
+    }
+    else
+    {
+        outputImgL[idx] = dL;
+    }
 }
+
+__kernel void occlusion_filling(__global BYTE *readyOutputImg, __global BYTE *inputImageL, int width, int height, int range, int win_size)
+{
+    int i = get_global_id(1);
+    int j = get_global_id(0);
+
+    float pixval;
+    int left, right, top, bottom, x, y, rp, rn;
+
+    if ((i - (win_size -1)/2 < 0) || ( i + (win_size -1)/2 >= height)|| ( j - (win_size -1)/2 < 0) || (j + (win_size -1)/2 >= width))
+    {
+        return;
+    }
+
+    int idx = j+i*width;
+    if(inputImageL[idx] == 0.0f)
+    {
+        for (rp = 0, rn = 0; rp < range && rn > -range; rp++, rn--)
+        {
+            left = j + rn;
+            right = j + rp;
+            top = i + rn;
+            bottom = i + rp;
+            if(left<0){left = 0;}
+            if(right>=width){right = width - 1;}
+            if(top < 0){top = 0;}
+            if(bottom>=height){bottom = height - 1;}
+                
+            pixval = 0.0f;
+
+            for (x=left; x<=right; x++)
+            {
+                for (y = top; y <= bottom; y++)
+                {
+                    if (inputImageL[x + y*width] > pixval)
+                    {
+                        pixval = inputImageL[x + y*width];
+                    }
+                }
+            }
+            if(pixval != 0.0f){
+                readyOutputImg[idx] = pixval;
+                break;
+            }
+        }
+    } else {
+        readyOutputImg[idx] = inputImageL[idx];
+    }
+}
+
--- phase_5/opencl/src/main.cpp
+++ phase_5/opencl/src/main.cpp
@@ -3,14 +3,19 @@
 #include <vector>
 #include <CL/cl.h>
 #include "lodepng.h"
+#include <sys/time.h>
 
 #define WIDTH 2940
 #define HEIGHT 2016
+#define WINDOW 13
 #define CHANNELS 4
 #define MAX_DISPARITY 64
-#define WINDOW 5
+#define RESIZE_FACTOR 4
 
 typedef unsigned char BYTE;
+
+struct timeval t[6];
+struct timeval pixeltime[2];
 
 std::string loadKernelSource(const char* filename)
 {
@@ -35,12 +40,23 @@ int main()
 
     unsigned int w = WIDTH;
     unsigned int h = HEIGHT;
+    unsigned int d = MAX_DISPARITY;
+    int window = WINDOW;
+    unsigned int resize_factor = RESIZE_FACTOR;
+    unsigned int resized_w = w / resize_factor;
+    unsigned int resized_h = h / resize_factor;
 
     //unsigned char* inputImageLeft = NULL;
     //unsigned char* inputImageRight = NULL;
 
     unsigned char* inputImageLeft = (unsigned char*)malloc(w * h * 4 * sizeof(unsigned char));
     unsigned char* inputImageRight = (unsigned char*)malloc(w * h * 4 * sizeof(unsigned char));
+    unsigned char* tinyGrayLeft = (unsigned char *)malloc(w/resize_factor * h/resize_factor * sizeof(unsigned char));
+    unsigned char* tinyGrayRight = (unsigned char *)malloc(w/resize_factor * h/resize_factor * sizeof(unsigned char));
+    unsigned char* disparityL = (unsigned char *)calloc(w / resize_factor * h / resize_factor, sizeof(unsigned char));
+    unsigned char* disparityR = (unsigned char *)calloc(w / resize_factor * h / resize_factor, sizeof(unsigned char));
+    unsigned char *crosscheckedL = (unsigned char *)calloc(w/resize_factor * h/resize_factor, sizeof(unsigned char));
+    unsigned char *occlusionL = (unsigned char *)calloc(w/resize_factor * h/resize_factor, sizeof(unsigned char));
 
     unsigned error;
 
@@ -85,21 +101,21 @@ int main()
 
     /* ---------- BUFFERS ---------- */
 
-    size_t imageSize = w*h*CHANNELS;
+    size_t imageSize = w*h*CHANNELS * sizeof(unsigned char);
+    size_t tinyGrayImageSize = w/4 * h/4 * sizeof(unsigned char);
+
     cl_mem leftBuffer = clCreateBuffer(context,CL_MEM_READ_ONLY|CL_MEM_COPY_HOST_PTR,imageSize,inputImageLeft,&err);
     cl_mem rightBuffer = clCreateBuffer(context,CL_MEM_READ_ONLY|CL_MEM_COPY_HOST_PTR,imageSize,inputImageRight,&err);
-    std::vector<unsigned char> disparityL(w*h);
-    std::vector<unsigned char> disparityR(w*h);
+    cl_mem tinyGrayBufferL = clCreateBuffer(context, CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR, tinyGrayImageSize, tinyGrayLeft, &err);
+    cl_mem tinyGrayBufferR = clCreateBuffer(context, CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR, tinyGrayImageSize, tinyGrayRight, &err);
 
-    cl_mem disparityBuffer = clCreateBuffer(context,CL_MEM_WRITE_ONLY,w*h*sizeof(unsigned char),NULL,&err);
+    cl_mem disparityBufferL = clCreateBuffer(context,CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR,tinyGrayImageSize,disparityL,&err);
+    cl_mem disparityBufferR = clCreateBuffer(context,CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR,tinyGrayImageSize,disparityR,&err);
+    cl_mem crosscheckedBufL = clCreateBuffer(context, CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR, tinyGrayImageSize, crosscheckedL, &err); 
+    cl_mem occlusionBufferL = clCreateBuffer(context, CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR, tinyGrayImageSize, occlusionL, &err); 
 
-    size_t global[2] = {w,h};
-
-    int disp_sign = 1;
-
-    /* ---------- KERNEL 1 FAST ---------- */
-
-    std::string kernelSource1 = loadKernelSource("kernel_fast.cl");
+    // Load kernel functions
+    std::string kernelSource1 = loadKernelSource("kernels.cl");
     const char* src1 = kernelSource1.c_str();
     size_t src1size = kernelSource1.length();
 
@@ -114,20 +130,79 @@ int main()
         std::cout<<log.data()<<std::endl;
         return -1;
     }
+    /* ---------- KERNEL 1 RESIZE AND GRAYSCALE ---------- */
 
-    cl_kernel kernel1 = clCreateKernel(program1,"zncc_fast",&err);
+    gettimeofday(&t[0], NULL);
+    size_t global1[2] = {size_t(resized_h), size_t(resized_w)};
+
+    cl_kernel kernel1 = clCreateKernel(program1,"resize_grayscale_image",&err);
     clSetKernelArg(kernel1,0,sizeof(cl_mem),&leftBuffer);
     clSetKernelArg(kernel1,1,sizeof(cl_mem),&rightBuffer);
-    clSetKernelArg(kernel1,2,sizeof(cl_mem),&disparityBuffer);
-    clSetKernelArg(kernel1,3,sizeof(int),&disp_sign);
-    clSetKernelArg(kernel1,4,sizeof(int),&w);
-    clSetKernelArg(kernel1,5,sizeof(int),&h);
+    clSetKernelArg(kernel1,2,sizeof(cl_mem),&tinyGrayBufferL);
+    clSetKernelArg(kernel1,3,sizeof(cl_mem),&tinyGrayBufferR);
+    clSetKernelArg(kernel1,4,sizeof(cl_uchar),&resize_factor);
+    clSetKernelArg(kernel1,5,sizeof(cl_uint),&w);
 
-    clEnqueueNDRangeKernel(queue,kernel1,2,NULL,global,NULL,0,NULL,NULL);
+    clEnqueueNDRangeKernel(queue,kernel1,2,NULL,global1,NULL,0,NULL,NULL);
     clFinish(queue);
 
-    clEnqueueReadBuffer(queue,disparityBuffer,CL_TRUE,0,w*h*sizeof(unsigned char),disparityL.data(),0,NULL,NULL);
+    clEnqueueReadBuffer(queue,tinyGrayBufferL,CL_TRUE,0,w/resize_factor*h/resize_factor*sizeof(unsigned char),tinyGrayLeft,0,NULL,NULL);
+    clEnqueueReadBuffer(queue,tinyGrayBufferR,CL_TRUE,0,w/resize_factor*h/resize_factor*sizeof(unsigned char),tinyGrayRight,0,NULL,NULL);
 
+    lodepng_encode_file("../Output_images/tinyGrayLeft.png", tinyGrayLeft, w/resize_factor, h/resize_factor, LCT_GREY, 8); 
+    lodepng_encode_file("../Output_images/tinyGrayRight.png", tinyGrayRight, w/resize_factor, h/resize_factor, LCT_GREY, 8); 
+
+
+    gettimeofday(&t[1], NULL);
+    int zero = 0;
+    cl_mem debugBuf = clCreateBuffer(context, CL_MEM_WRITE_ONLY, 8192, NULL, &err);
+    cl_mem debugIndex = clCreateBuffer(context, CL_MEM_READ_WRITE, sizeof(int), NULL, &err);
+    clEnqueueWriteBuffer(queue, debugIndex, CL_TRUE, 0, sizeof(int), &zero, 0, NULL, NULL);
+    char buffer[8192];
+
+    /*-------- DEBUG KERNEL TESTING --------*/
+    //cl_kernel testKernel = clCreateKernel(program1, "testKernel", &err);
+    //clSetKernelArg(testKernel, 0, sizeof(cl_mem), &debugBuf);
+    //clSetKernelArg(testKernel, 1, sizeof(cl_mem), &debugIndex);
+    //size_t testGlobalSize[2] = {16,16};
+    //clEnqueueNDRangeKernel(queue, testKernel, 2, NULL, testGlobalSize, NULL, 0, NULL, NULL); 
+    //clFinish(queue);
+    //clEnqueueReadBuffer(queue, debugBuf, CL_TRUE, 0, 8192, buffer, 0, NULL, NULL);
+    //printf("%s\n", buffer);
+
+    /* ---------- KERNEL 1 FAST ---------- */
+    //size_t global2[3] = {size_t(resized_w),size_t(resized_h),size_t(d)};
+    size_t global2[2] = {size_t(resized_w),size_t(resized_h)};
+    //size_t global2[2] = {((resized_w + 15) / 16) * 16,((resized_h + 15) / 16) * 16};
+    //size_t local2[2] = {16, 16};
+    int disp_sign = 1;
+
+    //std::string kernelSource1 = loadKernelSource("kernel_fast.cl");
+
+    clEnqueueWriteBuffer(queue, debugIndex, CL_TRUE, 0, sizeof(int), &zero, 0, NULL, NULL);
+
+    cl_kernel kernel2 = clCreateKernel(program1,"zncc_fast",&err);
+    clSetKernelArg(kernel2,0,sizeof(cl_mem),&tinyGrayBufferL);
+    clSetKernelArg(kernel2,1,sizeof(cl_mem),&tinyGrayBufferR);
+    clSetKernelArg(kernel2,2,sizeof(cl_mem),&disparityBufferL);
+    clSetKernelArg(kernel2,3,sizeof(cl_int),&disp_sign);
+    clSetKernelArg(kernel2,4,sizeof(cl_int),&resized_w);
+    clSetKernelArg(kernel2,5,sizeof(cl_int),&resized_h);
+    clSetKernelArg(kernel2,6, sizeof(cl_int),&window); 
+    clSetKernelArg(kernel2,7,sizeof(cl_mem),&debugBuf);
+    clSetKernelArg(kernel2,8,sizeof(cl_mem),&debugIndex);
+
+
+    clEnqueueNDRangeKernel(queue,kernel2,2,NULL,global2, NULL,0,NULL,NULL);
+    clFinish(queue);
+
+    clEnqueueReadBuffer(queue,disparityBufferL,CL_TRUE,0,resized_w
+            *resized_h*sizeof(unsigned char),disparityL,0,NULL,NULL);
+
+    clEnqueueReadBuffer(queue, debugBuf, CL_TRUE, 0, 8192, buffer, 0, NULL, NULL);
+    printf("%s\n", buffer);
+
+    /*
     std::vector<unsigned char> output1(w*h*4);
     for(int i=0;i<w*h;i++)
     {
@@ -137,60 +212,32 @@ int main()
         output1[i*4+2]=d;
         output1[i*4+3]=255;
     }
-    lodepng_encode32_file("../Output_images/depth1.png",output1.data(),w,h);
-    std::cout<<"Depth map 1 saved\n";
-
-    /* ---------- KERNEL 2 CPU STYLE ---------- */
-    /*
-    std::string kernelSource2 = loadKernelSource("kernel_cpu.cl");
-    const char* src2 = kernelSource2.c_str();
-    size_t src2size = kernelSource2.length();
-
-    cl_program program2 = clCreateProgramWithSource(context,1,&src2,&src2size,&err);
-    err = clBuildProgram(program2,1,&device,NULL,NULL,NULL);
-    if(err!=CL_SUCCESS)
-    {
-        size_t log_size;
-        clGetProgramBuildInfo(program2,device,CL_PROGRAM_BUILD_LOG,0,NULL,&log_size);
-        std::vector<char> log(log_size);
-        clGetProgramBuildInfo(program2,device,CL_PROGRAM_BUILD_LOG,log_size,log.data(),NULL);
-        std::cout<<log.data()<<std::endl;
-        return -1;
-    }
-
-    cl_kernel kernel2 = clCreateKernel(program2,"zncc_cpu",&err);
-
-    int win_size = WINDOW*2+1;
-    int min_d = 0;
-    int max_d = MAX_DISPARITY;
-
-    clSetKernelArg(kernel2,0,sizeof(cl_mem),&leftBuffer);
-    clSetKernelArg(kernel2,1,sizeof(cl_mem),&rightBuffer);
-    clSetKernelArg(kernel2,2,sizeof(cl_mem),&disparityBuffer);
-    clSetKernelArg(kernel2,3,sizeof(int),&w);
-    clSetKernelArg(kernel2,4,sizeof(int),&h);
-    clSetKernelArg(kernel2,5,sizeof(int),&win_size);
-    clSetKernelArg(kernel2,6,sizeof(int),&min_d);
-    clSetKernelArg(kernel2,7,sizeof(int),&max_d);
-
     */
+    //lodepng_encode32_file("../Output_images/depth1.png",output1.data(),w,h);
+
+    lodepng_encode_file("../Output_images/depth1.png", disparityL, resized_w, resized_h, LCT_GREY, 8);
+    std::cout<<"Depth map 1 saved\n";
 
     disp_sign = -1;
 
-    kernel1 = clCreateKernel(program1,"zncc_fast",&err);
-    clSetKernelArg(kernel1,0,sizeof(cl_mem),&rightBuffer);
-    clSetKernelArg(kernel1,1,sizeof(cl_mem),&leftBuffer);
-    clSetKernelArg(kernel1,2,sizeof(cl_mem),&disparityBuffer);
-    clSetKernelArg(kernel1,3,sizeof(int),&disp_sign);
-    clSetKernelArg(kernel1,4,sizeof(int),&w);
-    clSetKernelArg(kernel1,5,sizeof(int),&h);
-    
+    kernel2 = clCreateKernel(program1,"zncc_fast",&err);
+    clSetKernelArg(kernel2,0,sizeof(cl_mem),&tinyGrayBufferR);
+    clSetKernelArg(kernel2,1,sizeof(cl_mem),&tinyGrayBufferL);
+    clSetKernelArg(kernel2,2,sizeof(cl_mem),&disparityBufferR);
+    clSetKernelArg(kernel2,3,sizeof(cl_int),&disp_sign);
+    clSetKernelArg(kernel2,4,sizeof(cl_int),&resized_w);
+    clSetKernelArg(kernel2,5,sizeof(cl_int),&resized_h);
+    clSetKernelArg(kernel2,6, sizeof(cl_int),&window); 
+    clSetKernelArg(kernel2,7,sizeof(cl_mem),&debugBuf);
+    clSetKernelArg(kernel2,8,sizeof(cl_mem),&debugIndex);
 
-    clEnqueueNDRangeKernel(queue,kernel1,2,NULL,global,NULL,0,NULL,NULL);
+    clEnqueueNDRangeKernel(queue,kernel2,2,NULL,global2, NULL,0, NULL,NULL);
     clFinish(queue);
 
-    clEnqueueReadBuffer(queue,disparityBuffer,CL_TRUE,0,w*h*sizeof(unsigned char),disparityR.data(),0,NULL,NULL);
+    clEnqueueReadBuffer(queue,disparityBufferR,CL_TRUE,0,resized_w*resized_h*sizeof(unsigned char),disparityR,0,NULL,NULL);
+    
 
+    /*
     std::vector<unsigned char> output2(w*h*4);
     for(int i=0;i<w*h;i++)
     {
@@ -200,22 +247,112 @@ int main()
         output2[i*4+2]=d;
         output2[i*4+3]=255;
     }
-    lodepng_encode32_file("../Output_images/depth2.png",output2.data(),w,h);
+    */
+    //lodepng_encode32_file("../Output_images/depth2.png",output2.data(),w,h);
+    lodepng_encode_file("../Output_images/depth2.png", disparityR, resized_w, resized_h, LCT_GREY, 8);
+    
     std::cout<<"Depth map 2 saved\n";
+    gettimeofday(&t[2], NULL);
+
+    int threshold = 20;
+    
+    clEnqueueWriteBuffer(queue, debugIndex, CL_TRUE, 0, sizeof(int), &zero, 0, NULL, NULL);
+
+
+    cl_kernel kernel3 = clCreateKernel(program1, "cross_checking", &err);
+    clSetKernelArg(kernel3, 0, sizeof(cl_mem), &disparityBufferL);
+    clSetKernelArg(kernel3, 1, sizeof(cl_mem), &disparityBufferR);
+    clSetKernelArg(kernel3, 2, sizeof(cl_mem), &crosscheckedBufL);
+    clSetKernelArg(kernel3, 3, sizeof(cl_int), &resized_w);
+    clSetKernelArg(kernel3, 4, sizeof(cl_int), &resized_h);
+    clSetKernelArg(kernel3, 5, sizeof(cl_int), &threshold);
+    clSetKernelArg(kernel3, 6, sizeof(cl_int), &window);
+    clSetKernelArg(kernel3,7,sizeof(cl_mem),&debugBuf);
+    clSetKernelArg(kernel3,8,sizeof(cl_mem),&debugIndex);
+
+    clEnqueueNDRangeKernel(queue,kernel3,2,NULL,global2, NULL,0, NULL,NULL);
+    clFinish(queue);
+
+    clEnqueueReadBuffer(queue,crosscheckedBufL,CL_TRUE,0,resized_w*resized_h*sizeof(unsigned char),crosscheckedL,0,NULL,NULL);
+    clEnqueueReadBuffer(queue, debugBuf, CL_TRUE, 0, 8192, buffer, 0, NULL, NULL);
+    printf("%s\n", buffer);
+
+
+    lodepng_encode_file("../Output_images/cross_checked.png", crosscheckedL, resized_w, resized_h, LCT_GREY, 8);
+    printf("Cross checked output image saved.\n");
+
+
+    gettimeofday(&t[3], NULL);
+
+    int neighbourhood_range = 50;
+
+    cl_kernel kernel4 = clCreateKernel(program1, "occlusion_filling", &err);
+    clSetKernelArg(kernel4, 0, sizeof(cl_mem), &occlusionBufferL);
+    clSetKernelArg(kernel4, 1, sizeof(cl_mem), &crosscheckedBufL);
+    clSetKernelArg(kernel4, 2, sizeof(cl_int), &resized_w);
+    clSetKernelArg(kernel4, 3, sizeof(cl_int), &resized_h);
+    clSetKernelArg(kernel4, 4, sizeof(cl_int), &neighbourhood_range);
+    clSetKernelArg(kernel4, 5, sizeof(cl_int), &window);
+
+    clEnqueueNDRangeKernel(queue,kernel4,2,NULL,global2, NULL,0, NULL,NULL);
+    clFinish(queue);
+
+    clEnqueueReadBuffer(queue,occlusionBufferL,CL_TRUE,0,resized_w*resized_h*sizeof(unsigned char),occlusionL,0,NULL,NULL);
+
+    lodepng_encode_file("../Output_images/occlusion_filled.png", occlusionL, resized_w, resized_h, LCT_GREY, 8);
+    printf("Occlusion filled output image saved.\n");
+
+
+    gettimeofday(&t[4], NULL);
+    gettimeofday(&t[5], NULL);
+
+    // Profiling
+    long seconds_1 = t[1].tv_sec - t[0].tv_sec;
+    long useconds_1 = t[1].tv_usec - t[0].tv_usec;
+    if(useconds_1 < 0){seconds_1--; useconds_1 += 1e6;}
+    long milliseconds_1 = seconds_1 * 1000.0 + useconds_1 / 1000.0;
+
+    long seconds_2 = t[2].tv_sec - t[1].tv_sec;
+    long useconds_2 = t[2].tv_usec - t[1].tv_usec;
+    if(useconds_2 < 0){seconds_2--; useconds_2 += 1e6;}
+    long milliseconds_2 = seconds_2 * 1000.0 + useconds_2 / 1000.0;
+
+    long seconds_3 = t[3].tv_sec - t[2].tv_sec;
+    long useconds_3 = t[3].tv_usec - t[2].tv_usec;
+    if(useconds_3 < 0){seconds_3--; useconds_3 += 1e6;}
+    long milliseconds_3 = seconds_3 * 1000.0 + useconds_3 / 1000.0;
+
+    long seconds_4 = t[4].tv_sec - t[3].tv_sec;
+    long useconds_4 = t[4].tv_usec - t[3].tv_usec;
+    if(useconds_4 < 0){seconds_4--; useconds_4 += 1e6;}
+    long milliseconds_4 = seconds_4 * 1000.0 + useconds_4 / 1000.0;
+
+    printf("Grayscale and resize (milliseconds): %ld\n", milliseconds_1);
+    printf("Zncc (milliseconds): %ld\n", milliseconds_2);
+    printf("Croschecking (milliseconds): %ld\n", milliseconds_3);
+    printf("Occlusion filling (milliseconds): %ld\n", milliseconds_4);
 
     /* ---------- CLEANUP ---------- */
     clReleaseKernel(kernel1);
+    clReleaseKernel(kernel2);
     clReleaseProgram(program1);
-    //clReleaseKernel(kernel2);
-    //clReleaseProgram(program2);
     clReleaseMemObject(leftBuffer);
     clReleaseMemObject(rightBuffer);
-    clReleaseMemObject(disparityBuffer);
+    clReleaseMemObject(tinyGrayBufferL);
+    clReleaseMemObject(tinyGrayBufferR);
+    clReleaseMemObject(disparityBufferL);
+    clReleaseMemObject(disparityBufferR);
     clReleaseCommandQueue(queue);
     clReleaseContext(context);
 
     free(inputImageLeft);
     free(inputImageRight);
+    free(tinyGrayLeft);
+    free(tinyGrayRight);
+    free(disparityL);
+    free(disparityR);
+    free(crosscheckedL);
+    free(occlusionL);
 
     return 0;
 }
